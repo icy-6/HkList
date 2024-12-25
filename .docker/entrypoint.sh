e337361b390689636fb0271e472571ef41813b74
huankong233 <huank0ng@foxmail.com>
2024-12-25 14:43:18 +0800

    [*] 修正入口文件

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,28 +3,20 @@
 # 变量
 www_path="/var/www/html"
 latest_path="/var/www/HkList"
-commands_path="/app/Console/Commands"
 
 echo "检查目录映射是否正确" && \
 if [ -d "$www_path" ]; then
     cd $www_path || exit
-    if [ ! "$(ls -A $www_path)" ]; then
-        echo "文件夹为空,导入文件"
-        cp -a $latest_path/. $www_path
-    fi
+    echo "映射路径正确"
 else
     echo "没有正确映射路径"
     exit
 fi
 
-echo "导入Commands文件夹"
-rm -rf $www_path$commands_path
-mkdir -p $www_path$commands_path
-cp -a $latest_path$commands_path/. $www_path$commands_path
-
-echo "导入新的依赖"
-rm -rf $www_path/vendor
-cp -a $latest_path/vendor/. $www_path/vendor
+echo "导入文件"
+rm -rf $www_path
+mkdir -p $www_path
+cp -a $latest_path/. $www_path
 
 echo "启动更新程序"
 cd $latest_path || exit
