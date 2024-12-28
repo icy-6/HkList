72fd67b711cccfce8f59612e6dc486b0b7a41e62
huankong233 <huank0ng@foxmail.com>
2024-12-28 10:43:47 +0800

    [+] 更新前端 `2.0.13` [*] 修复自动更新导致数据库报错

--- public/assets/js/index-Bli7gMPY.js
+++
@@ -1,5 +0,0 @@
-import{_ as a}from"./index-Dw3IExJB.js";import{R as o}from"./index-DxT_05GM.js";/**
- * tdesign v1.10.4
- * (c) 2024 tdesign
- * @license MIT
- */var t=o(a);export{t as D};
---
+++ public/assets/js/index-DNAHcPz3.js
@@ -0,0 +1,5 @@
+import{_ as a}from"./index-B-Lya6Pj.js";import{R as o}from"./index-DmOSGg5I.js";/**
+ * tdesign v1.10.4
+ * (c) 2024 tdesign
+ * @license MIT
+ */var t=o(a);export{t as D};
