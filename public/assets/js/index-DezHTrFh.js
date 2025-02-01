8db4f11adac2c8763a76bce75d5e1f92ed49cbd6
huankong233 <huank0ng@foxmail.com>
2025-02-01 15:00:35 +0800

    [*] 基本趋于稳定 [*] 优化 V6 解析模式 [+] 增加自定义 JS 脚本 [*] 更新前端 `2.1.0`

--- public/assets/js/index-DezHTrFh.js
+++
@@ -1,5 +0,0 @@
-import{_ as a}from"./index-yl2bWJuX.js";import{S as o}from"./index-DHXQ7Ria.js";/**
- * tdesign v1.10.6
- * (c) 2024 tdesign
- * @license MIT
- */var t=o(a);export{t as D};
--- resources/views/App.blade.php
+++ resources/views/App.blade.php
@@ -5,3 +5,10 @@
         echo '未获取到页面内容,请检查public目录下是否存在编译后的前端项目!!!';
     }
 @endphp
+
+{{--插入自定义脚本--}}
+@if(config("hklist.general.custom_script") !== "")
+    <script>
+        {!! config("hklist.general.custom_script") !!}
+    </script>
+@endif
