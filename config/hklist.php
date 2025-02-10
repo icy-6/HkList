b814b206eca9af8d3a6409f561623afe862342f8
huankong233 <huank0ng@foxmail.com>
2025-02-10 17:23:55 +0800

    [+] 支持配置客户端是否允许下载文件夹

--- config/hklist.php
+++ config/hklist.php
@@ -5,7 +5,7 @@ return [
     "fake_wx_user_agent" => env("HKLIST_FAKE_WX_USER_AGENT", "Mozilla/5.0 (Linux; Android 7.1.1; MI 6 Build/NMF26X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/6.2 TBS/043807 Mobile Safari/537.36 MicroMessenger/6.6.1.1220(0x26060135) NetType/4G Language/zh_CN MicroMessenger/6.6.1.1220(0x26060135) NetType/4G Language/zh_CN miniProgram"),
     "fake_cookie" => env("HKLIST_FAKE_COOKIE", "BAIDUID=A4FDFAE43DDBF7E6956B02F6EF715373:FG=1; BAIDUID_BFESS=A4FDFAE43DDBF7E6956B02F6EF715373:FG=1; newlogin=1"),
 
-    "version" => "2.1.5",
+    "version" => "2.1.6",
 
     "general" => [
         "admin_password" => env("HKLIST_ADMIN_PASSWORD", ""),
@@ -34,6 +34,7 @@ return [
         "parse_mode" => (int)env("HKLIST_PARSE_MODE", 1),
         "user_agent" => env("HKLIST_USER_AGENT", ""),
         "use_exploit" => env("HKLIST_USE_EXPLOIT", false),
+        "allow_folder" => (bool)env("HKLIST_ALLOW_FOLDER", false),
     ],
     "proxy" => [
         "enable" => (bool)env("HKLIST_PROXY_ENABLE", false),
