d8190571aba6a61d9e867ab10efddc3b6fe4109f
huankong233 <huank0ng@foxmail.com>
2025-04-05 23:37:30 +0800

    [*] 更新前端 `2.1.15` [*] 修复 Docker 无法运行 ocr

--- public/assets/js/index-DXSvMXEy.js
+++
@@ -1,5 +0,0 @@
-import{_ as o,a as r}from"./index-FlGTPdZM.js";import{R as a}from"./index-BaVJ77xG.js";/**
- * tdesign v1.11.5
- * (c) 2025 tdesign
- * @license MIT
- */var s=a(o);a(r);export{s as D};
