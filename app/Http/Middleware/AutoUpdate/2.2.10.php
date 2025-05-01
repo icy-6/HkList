1289a68341a199831bdd0cc8c07561952e2aa429
huankong233 <huank0ng@foxmail.com>
2025-05-02 00:28:54 +0800

    [*] 优化自动更新

--- app/Http/Middleware/AutoUpdate/2.2.10.php
+++ app/Http/Middleware/AutoUpdate/2.2.10.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Schema;
 // 2.2.10 修复文件列表表 fs_id 索引
 $indexes = DB::select('SHOW INDEXES FROM file_lists WHERE Column_name = ?', ['fs_id']);
 if (empty($indexes)) {
-    DB::statement("DELETE t1 FROM file_lists t1 JOIN file_lists t2 ON t1.fs_id = t2.fs_id AND t1.id < t2.id;");
     Schema::table('file_lists', function (Blueprint $table) {
         $table->string('fs_id', 255)->unique()->change();
     });
